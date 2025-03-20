d83e2549173ab68352330555191137b9c7920e1a
bain-liao-1 <liao.bain@stu.ocsb.ca>
2025-03-20 17:23:05 +0000

    fixed comment

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Bain Liao
-// Created on: Feb 2025
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
 "use strict"
